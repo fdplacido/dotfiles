ff8a08de012a9bcda924fdff6c9c34755d4b2e60
capravictoriae <pmail314@gmail.com>
2018-03-04 14:14:26 +0100

    Updated get files script again

--- get_files.py
+++ get_files.py
@@ -5,32 +5,33 @@ import shutil
 def get_username():
     return pwd.getpwuid( os.getuid() )[ 0 ]
 
-user = get_username()
-homepath = '/home/' + user + '/'
-
 def copy_file(source, dest):
   if not os.path.exists(dest):
     os.makedirs(dest)
   shutil.copy(source, dest)
 
+user = get_username()
+homepath = '/home/' + user + '/'
+cwd = str(os.getcwd())
+
 # Bashrc
 source = homepath + '.bashrc'
-dest = str(os.getcwd()) + '/bashrc/'
+dest = cwd + '/bashrc/'
 copy_file(source, dest)
 
 # tmux
 source = homepath + '.tmux.conf'
-dest = str(os.getcwd()) + '/tmux/'
+dest = cwd + '/tmux/'
 copy_file(source, dest)
 
 # vim
 source = homepath + '.vimrc'
-dest = str(os.getcwd()) + '/vim/'
+dest = cwd + '/vim/'
 copy_file(source, dest)
 
 # sublime 3 (and plugins) settings
 source = homepath + '.config/sublime-text-3/Packages/User/Preferences.sublime-settings'
-dest = str(os.getcwd()) + '/sublime/'
+dest = cwd + '/sublime/'
 copy_file(source, dest)
 # key bindings
 source = homepath + '.config/sublime-text-3/Packages/User/Default (Linux).sublime-keymap'
@@ -41,15 +42,15 @@ copy_file(source, dest)
 
 # Firefox theme
 source = homepath + '.mozilla/firefox/ttoulgrc.default/chrome/userChrome.css'
-dest = str(os.getcwd()) + '/firefox/'
+dest = cwd + '/firefox/'
 copy_file(source, dest)
 
 # Typora
 source = homepath + '.config/Typora/conf/conf.user.json'
-dest = str(os.getcwd()) + '/typora/conf/'
+dest = cwd + '/typora/conf/'
 copy_file(source, dest)
 # My github theme (numbered)
 source = homepath + '.config/Typora/themes/github-numbered.css'
-dest = str(os.getcwd()) + '/typora/themes/'
+dest = cwd + '/typora/themes/'
 copy_file(source, dest)
 
